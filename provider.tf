cfe8cbb1c6297d24119d499c4966bdbe94c01eb1
Rahulavishetty <86112028+Rahulavishetty@users.noreply.github.com>
2025-03-24 15:59:41 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "oci" {
-  tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaqitjiiy2rwxnzhpcdxlvzmxkl2x7ki6doky2jqptm7uhfz7u6gpa"
-  user_ocid        = "ocid1.user.oc1..aaaaaaaap4qb2nbsd6ookqihi7wbpi4i2qtllstlr4qalvo4kg6hdrp575uq"
-  private_key_path = "/root/.oci/oci_api_key.pem"
-  fingerprint      = "18:72:96:7a:84:55:e6:2f:32:20:83:95:c9:db:bd:56"
-  region           = "ap-mumbai-1"
+  tenancy_ocid     = ###
+  user_ocid        = ###
+  private_key_path = ###
+  fingerprint      = ###
+  region           = ###
 }
 
